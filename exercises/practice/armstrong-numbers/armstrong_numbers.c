e3280fdab7f2de34090a0789c3382dba0c1a4e9d
Stephen Wall <stephen@kmcmh.com>
2023-05-19 13:46:21 -0600

    attempted isogram and solved armstrong_numbers

--- exercises/practice/armstrong-numbers/armstrong_numbers.c
+++ exercises/practice/armstrong-numbers/armstrong_numbers.c
@@ -1,1 +1,41 @@
 #include "armstrong_numbers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+bool is_armstrong_number1(int i){
+	char digits[12];
+	int pow = sprintf(digits, "%d", i);
+	int sum = 0;
+	for(int j = 0; j < pow; j++){
+    int exp = 1;
+		for(int k = 0; k < pow; k++){
+			exp *= digits[j] - '0';	
+		}
+		sum += exp;
+	}	
+	return sum == i;
+}
+
+bool is_armstrong_number(int i){
+    int digit_count = 0;
+    int temp_i = i;
+
+    int digits[11];
+    while(temp_i > 0){
+        digits[digit_count] = temp_i % 10;
+        temp_i /= 10;
+        digit_count++;
+    }
+    printf("digit_count= %d\n",digit_count);
+    int sum = 0;
+    for(int j = 0;j<digit_count; j++){
+        printf("digits[%d]= %d\n",j,digits[j]);
+        int pow = 1;
+        for(int k = 0; k < digit_count; k++){
+            pow *= digits[j];
+        }
+        sum += pow;
+    }
+
+    return i == sum;
+}
--- exercises/practice/isogram/isogram.c
+++ exercises/practice/isogram/isogram.c
@@ -1,1 +1,16 @@
 #include "isogram.h"
+
+
+bool is_isogram(const char phrase[]){
+    if(!phrase)
+        return false;
+
+    uint32_t letter_flags = 0;
+
+    while(*phrase){
+        char letter = *phrase;
+        if(letter >= 'a' && letter <= 'z'){
+        } else if(letter >= 'A' && letter <= 'Z') {
+        }
+    }
+}
